c520423e0cf3caa9e62de7e2879006142a4888c4
andrii <andriykonyuk@gmail.com>
2018-01-11 12:15:45 +0200

    Test deploy to pythonanywhere

--- curr_conv/settings.py
+++ curr_conv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')d)5r^i%$_l1lr!!s4gfkj#3#g6h8g2v@2x1!@j_3c0$p&phw9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [u'AndriyKonyuk.pythonanywhere.com']
 
 
 # Application definition
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'curr_conv.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cur_conv',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'cur_conv',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
